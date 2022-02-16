6769595827dc06a6a26fd998e7038e39875f408e
Varun Bhatt <varunbhatt@Varuns-MacBook-Pro.local>
2022-02-16 13:08:46 +0530

    file db

--- app/products/database.py
+++ app/products/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'mysql://admin:iMUAUlu3@mysql-68971-0.cloudclusters.net:10284/wallart_backend'
+# SQLALCHAMY_DATABASE_URL = 'mysql://admin:iMUAUlu3@mysql-68971-0.cloudclusters.net:10284/wallart_backend'
 
-# SQLALCHAMY_DATABASE_URL = 'sqlite:///./products.db'
+SQLALCHAMY_DATABASE_URL = 'sqlite:///./products.db'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
 
