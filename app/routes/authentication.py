495ae093902cc908ee3edc275ef7ba780345ee64
Varun Bhatt <42655062+varunbhtt21@users.noreply.github.com>
2022-06-10 08:50:30 +0530

    user_id added

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -30,6 +30,8 @@ class Login(BaseModel):
 # def login(request : OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
 @router.post("")
+
+# def login(request : OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 def login(request : Login, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
@@ -39,4 +41,9 @@ def login(request : Login, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect Password")
 
     access_token = create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer"}
+
+    user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return {"access_token": access_token, "token_type": "bearer", "user_id":user.id}
