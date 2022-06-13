8e51681b0cf40d44d5d7ad3e7e348ee56fba060b
Varun Bhatt <42655062+varunbhtt21@users.noreply.github.com>
2022-06-13 08:56:50 +0530

    Key updated to live

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -8,6 +8,9 @@ from schemas import Product, Category, Payment
 from sqlalchemy.orm import Session
 import json
 import ast
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 
 router = APIRouter(
@@ -20,7 +23,14 @@ router = APIRouter(
 @router.post("", response_model=schemas.OrdersResponse)
 def orderPlaced(request: schemas.OrdersRequest, db: Session = Depends(database.get_db)):
 
-    client = razorpay.Client(auth=("rzp_test_xvp0YKDLxMR9mG", "zFrRXxSSzxbFYqSrUIwFfYph"))
+    os.getenv("CALENDY_CLIENT_ID")
+
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(override=True)
+    API_KEY = os.environ.get("API_KEY")
+    API_SECRET = os.environ.get("API_SECRET")
+
+    client = razorpay.Client(auth=(API_KEY, API_SECRET))
 
     data_string = json.dumps(request, default=lambda o: o.__dict__)
     data_dictionary = ast.literal_eval(data_string)
@@ -33,7 +43,7 @@ def orderPlaced(request: schemas.OrdersRequest, db: Session = Depends(database.g
 
 @router.post("/payment", response_model=str)
 def orderPlaced(request: schemas.Payment, db: Session = Depends(database.get_db)):
-    client = razorpay.Client(auth = ('rzp_test_V8hnZoWEdGP6Y0', 'nIgSCMYd7srvIs3x0aS9OxYs'))
+    client = razorpay.Client(auth = ('rzp_live_C80HDnSFL9Wxmb', 'nIgSCMYd7srvIs3x0aS9OxYs'))
 
     data_string = json.dumps(request, default=lambda o: o.__dict__)
     data_dictionary = ast.literal_eval(data_string)
