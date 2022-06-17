00f3d638bcc5f7aacc54004f6d9dcb5381d3b51f
Varun Bhatt <42655062+varunbhtt21@users.noreply.github.com>
2022-06-17 08:25:40 +0530

    cart fixed

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -31,8 +31,8 @@ class Login(BaseModel):
 
 @router.post("")
 
-# def login(request : OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-def login(request : Login, db: Session = Depends(get_db)):
+def login(request : OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+# def login(request : Login, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
@@ -47,3 +47,4 @@ def login(request : Login, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     return {"access_token": access_token, "token_type": "bearer", "user_id":user.id}
+
--- app/routes/cart.py
+++ app/routes/cart.py
@@ -1,4 +1,6 @@
+from cgi import print_exception
 from fastapi import APIRouter
+from models import Url
 import schemas, database, models
 from database import get_db
 from fastapi import FastAPI, Depends
@@ -6,6 +8,7 @@ from typing import Dict, List, Optional, Tuple
 from schemas import CartResponse, CartRequest
 from sqlalchemy.orm import Session
 from oauth2 import get_current_user
+from pydantic import BaseModel
 import routes
 
 router = APIRouter(
@@ -37,8 +40,30 @@ def checkout(request: List[CartRequest] ,user_id: int, db: Session = Depends(get
     return output
 
 
+class CartItem(BaseModel):
+    product_id : int 
+    name : str
+    url : str
+    price : int
+    quantity : int
 
-@router.get("/",response_model=List[CartResponse])
+    class Config:
+        orm_mode = True
+
+
+@router.get("/",response_model=List[CartItem])
 def allCartItems(db: Session = Depends(get_db)):
     cartItems = db.query(models.CartItems).all()
-    return cartItems
+    allItems = []
+    for item in cartItems:
+        product = db.query(models.Products).filter(models.Products.id == item.product_id).first()
+        new_cart_item = CartItem(product_id = item.product_id,
+                                name = product.name,
+                                url = product.urls[0].image_url,
+                                price = product.price,
+                                quantity = item.quantity)
+        allItems.append(new_cart_item)
+
+    return allItems
+
+ 
